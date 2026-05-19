151f5c8588b9f5ec2600dfedaf031a15c143726c
Nick <Yannick.Horden@outlook.com>
2026-05-19 13:28:42 +0200

    Update AbstractMinecartMixin to handle minecart derailing

--- common/src/main/java/dev/ryanhcode/sable/mixin/entity/entity_sublevel_collision/AbstractMinecartMixin.java
+++ common/src/main/java/dev/ryanhcode/sable/mixin/entity/entity_sublevel_collision/AbstractMinecartMixin.java
@@ -1,12 +1,15 @@
 package dev.ryanhcode.sable.mixin.entity.entity_sublevel_collision;
 
 import dev.ryanhcode.sable.Sable;
-import dev.ryanhcode.sable.index.SableTags;
+import dev.ryanhcode.sable.api.entity.EntitySubLevelUtil;
 import dev.ryanhcode.sable.sublevel.SubLevel;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseRailBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,22 +18,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractMinecart.class)
 public abstract class AbstractMinecartMixin extends Entity {
 
-
     public AbstractMinecartMixin(final EntityType<?> entityType, final Level level) {
         super(entityType, level);
     }
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void sable$postTick(final CallbackInfo ci) {
-        if (!this.getType().is(SableTags.DESTROY_WHEN_LEAVING_PLOT)) {
-            return;
-        }
-
         final SubLevel containingSubLevel = Sable.HELPER.getContaining(this);
+        if (containingSubLevel == null) return;
 
-        // Destroy us if we're in #sable:destroy_when_leaving_plot and we've left the plot
-        if (containingSubLevel != null && !this.getBoundingBox().intersects(containingSubLevel.getPlot().getBoundingBox().toAABB().inflate(0.5))) {
-            this.kill();
-        }
+        final BlockPos pos = this.blockPosition();
+        final BlockState here = this.level().getBlockState(pos);
+        final BlockState below = this.level().getBlockState(pos.below());
+
+        if (BaseRailBlock.isRail(here) || BaseRailBlock.isRail(below)) return;
+
+        EntitySubLevelUtil.kickEntity(containingSubLevel, this);
     }
 }
