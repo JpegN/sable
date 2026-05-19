db2ca264d7019b769df6aa8dfcc5e2ff14db76dc
Nick <Yannick.Horden@outlook.com>
2026-05-19 18:11:32 +0200

    fixed THIS WILL ACTUALLY WORK THIS TIME

--- common/src/main/java/dev/ryanhcode/sable/mixin/entity/entity_sublevel_collision/AbstractMinecartMixin.java
+++ common/src/main/java/dev/ryanhcode/sable/mixin/entity/entity_sublevel_collision/AbstractMinecartMixin.java
@@ -3,14 +3,12 @@ package dev.ryanhcode.sable.mixin.entity.entity_sublevel_collision;
 import dev.ryanhcode.sable.Sable;
 import dev.ryanhcode.sable.api.entity.EntitySubLevelUtil;
 import dev.ryanhcode.sable.sublevel.SubLevel;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseRailBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,16 +20,16 @@ public abstract class AbstractMinecartMixin extends Entity {
         super(entityType, level);
     }
 
+    @Shadow public abstract boolean isOnRails();
+
     @Inject(method = "tick", at = @At("TAIL"))
     private void sable$postTick(final CallbackInfo ci) {
+        if (this.level().isClientSide) return;
+
         final SubLevel containingSubLevel = Sable.HELPER.getContaining(this);
         if (containingSubLevel == null) return;
 
-        final BlockPos pos = this.blockPosition();
-        final BlockState here = this.level().getBlockState(pos);
-        final BlockState below = this.level().getBlockState(pos.below());
-
-        if (BaseRailBlock.isRail(here) || BaseRailBlock.isRail(below)) return;
+        if (this.isOnRails()) return;
 
         EntitySubLevelUtil.kickEntity(containingSubLevel, this);
     }
